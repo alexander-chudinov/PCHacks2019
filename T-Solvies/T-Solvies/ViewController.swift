711d17efafcdfd0d989681dbd1a0f9f0c1865322
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-05-04 14:47:32 -0400

    app updates

---
+++ T-Solvies/T-Solvies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T-Solvies
+//
+//  Created by Alexander Chudinov on 2019-05-04.
+//  Copyright © 2019 Alexander Chudinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
