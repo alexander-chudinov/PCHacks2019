cdd1dd67be3bb7efbf8ed5c0aa967fab119a8059
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-05-04 13:58:04 -0400

    abc

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
+    include('connect.php');
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
     $input = file_get_contents("php://input");
     $obj = json_decode($input);
     if($input!=null){
         echo "got : \n".var_dump($obj);
-        
     }else{
         echo "connected, no post detected";
     }
