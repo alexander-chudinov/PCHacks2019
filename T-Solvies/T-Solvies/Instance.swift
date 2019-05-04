d5dd6e6223322f5a9df2aa5ef9e34823530878ad
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-05-04 18:26:51 -0400

    minor fixes

---
+++ T-Solvies/T-Solvies/Instance.swift
@@ -0,0 +1,30 @@
+//
+//  Instance.swift
+//  T-Solvies
+//
+//  Created by Alexander Chudinov on 2019-05-04.
+//  Copyright © 2019 Alexander Chudinov. All rights reserved.
+//
+
+import UIKit
+
+class Instance: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
