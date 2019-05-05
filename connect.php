563395f130fe2a68a739afc8e07abb39b633e2c4
Alexander Chudinov <37562997+alexander-chudinov@users.noreply.github.com>
2019-05-05 07:01:01 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,10 +2,10 @@
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
 
-    define('DB_USER','visitor');
+    define('DB_USER','');
     define('DB_PSWD','');
-    define('DB_HOST','158.69.210.95:3306');
-    define('DB_NAME','tsolvies');
+    define('DB_HOST','');
+    define('DB_NAME','');
 
     $dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME)
     OR die("Error: " . mysqli_connect_error());
