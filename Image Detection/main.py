23fbad67d1c29d1aee8fa38e937f597d133c2646
Freeman Cheng <36014515+chfrn@users.noreply.github.com>
2019-05-05 03:44:47 -0400

    e

--- Image Detection/main.py
+++ Image Detection/main.py
@@ -1,18 +1,19 @@
 from imutils import paths
 import argparse
 import cv2
+import datetime
  
 def variance_of_laplacian(image):
 	return cv2.Laplacian(image, cv2.CV_64F).var()
- 
+
+img_num = 0
+file = open("data.txt", "a")
 for imagePath in sorted(paths.list_images("images")):
+	img_num = img_num+1
 	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	fm = variance_of_laplacian(gray)
-	text = "Not Blurry"
 	if fm < 120:
-		text = "Blurry"
-	#cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-		#cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-	#cv2.imshow("Image", image)
+		file.write(str(datetime.datetime.now())+";")
 	key = cv2.waitKey(0)
+file.close()
