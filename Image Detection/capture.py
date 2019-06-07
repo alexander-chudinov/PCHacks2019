89c9e667501aea2e6c5d576de0b1e9646966db62
Freeman Cheng <36014515+chfrn@users.noreply.github.com>
2019-06-07 10:53:05 -0400

    -
    -

--- Image Detection/capture.py
+++ Image Detection/capture.py
@@ -4,14 +4,11 @@ import cv2
 cap = cv2.VideoCapture(0)
 cnt = 0
 while(True):
-	cnt = cnt+1
-    # Capture frame-by-frame
-	ret, frame = cap.read()
-    # do what you want with frame
-    #  and then save to file
-	cv2.imwrite('images/%i.png' %(cnt), frame)
-	if cv2.waitKey(100) & 0xFF == ord('q'): # you can increase delay to 2 seconds here
-		break
+    cnt = cnt+1
+    ret, frame = cap.read()
+    cv2.imwrite('images/%i.png' %(cnt), frame)
+    if cv2.waitKey(60) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
--- Image Detection/main.py
+++ Image Detection/main.py
@@ -1,19 +1,18 @@
 from imutils import paths
-import argparse
 import cv2
 import datetime
  
 def variance_of_laplacian(image):
-	return cv2.Laplacian(image, cv2.CV_64F).var()
+    return cv2.Laplacian(image, cv2.CV_64F).var()
 
-img_num = 0
 file = open("data.txt", "a")
 for imagePath in sorted(paths.list_images("images")):
-	img_num = img_num+1
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	fm = variance_of_laplacian(gray)
-	if fm < 120:
-		file.write(str(datetime.datetime.now())+";")
-	key = cv2.waitKey(0)
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fm = variance_of_laplacian(gray)
+    if fm < 175:
+        file.write(str(datetime.datetime.now())+";")
+        cv2.imshow("Image", image)
+        key = cv2.waitKey(0)
+        cv2.destroyAllWindows()
 file.close()
